0c3de1dbe239316a29aeacb1403da040343a52d8
dercykar <cykaru@yandex.ru>
2022-07-03 19:05:42 +0300

    First commit

---
+++ service/src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SERVICE MODULE said - Hello World!" );
+    }
+}
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "MAIN APP said - Hello World!" );
+    }
+}
